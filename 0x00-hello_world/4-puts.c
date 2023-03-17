cf06f1afb2358b9222157a150ee708a5d0d0ce27
MohamedEltorky <mohamedeltorky1572001@gmail.com>
2023-03-16 17:49:29 -0700

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"programing is like building a multi lingual puzzle");
+    return (0);
+}
